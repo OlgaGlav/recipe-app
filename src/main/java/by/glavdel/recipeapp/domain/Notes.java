b06729aaadf4df35bf9e1bf708052fb46b00de35
Admin <admin>
2022-07-18 10:55:17 +0300

    @OneToOne

---
+++ src/main/java/by/glavdel/recipeapp/domain/Notes.java
@@ -0,0 +1,22 @@
+package by.glavdel.recipeapp.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+}
---
+++ src/main/java/by/glavdel/recipeapp/domain/Recipe.java
@@ -0,0 +1,31 @@
+package by.glavdel.recipeapp.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer serving;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+}
